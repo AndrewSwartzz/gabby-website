fb4028ac184b78d94ccd1bd51333d62b9e0aa607
andrew_swartzz <andreww_swartzz@gmail.com>
2025-03-30 22:50:13 -0400

    made months work

--- gabby.py
+++ gabby.py
@@ -17,21 +17,23 @@ def home():
     given_date = datetime.datetime.strptime(date_str, date_format).date()
     today = datetime.date.today()
     delta = today - given_date
+    months1 = delta.days//30
 
     date_str = '2024-09-06'
     date_format = "%Y-%m-%d"
     given_date = datetime.datetime.strptime(date_str, date_format).date()
     today = datetime.date.today()
     delta2 = today - given_date
+    months2 = delta2.days//30
 
-    return render_template('index.html', firstday=delta.days, dateday=delta2.days)
+    return render_template('index.html', firstday=delta.days, dateday=delta2.days, months1=months1, months2=months2)
 
 @app.route('/flirt')
 def flirt():
     flirtlines = ['Girl are you my daily flintstones gummies, because i could eat you up every single day ahaha',
                   'Girl are you a subway employee, because id pay you 5 dollars to make me a foot long ahaha',
                   'Girl are you the dying dehydrated basil plant on my window sill, because all i wanna do is see you get wet']
-    flirtline = flirtlines[randint(0,2)]
+    flirtline = flirtlines[randint(0,len(flirtlines)-1)]
     return render_template('pickupline.html', line=flirtline)
 
 
