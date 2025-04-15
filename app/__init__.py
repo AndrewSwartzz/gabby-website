1eeadfcb64e0a34c46719f026c2dbfde9a4311a2
andrew_swartzz <andreww_swartzz@gmail.com>
2025-04-15 12:56:35 -0400

    final birthday2

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
+import flask_mail
 from flask_mail import Mail
 from config import Config
 app = Flask(__name__)
