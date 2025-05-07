825b39434ab1acc72489a67c670aa61ec8dd05aa
andrew_swartzz <andreww_swartzz@gmail.com>
2025-05-06 21:25:46 -0400

    secret birthday route

--- gabby.py
+++ gabby.py
@@ -37,7 +37,7 @@ def home():
     days_since = (dt.now() - target_date).days
     age = (days_since//365)-1
 
-    if today.day == 15 and today.month == 4:
+    if today.day == 16 and today.month == 4:
         return render_template('birthday.html', firstday=delta.days, dateday=delta2.days, months1=months1, months2=months2
                                , daysmonths1=daysmonths1, daysmonths2=daysmonths2, days_since=days_since, age=age)
 
@@ -85,6 +85,34 @@ def coupons():
 @app.route('/reasons')
 def reasons():
     return render_template('love_reasons.html', title="LoveReasons")
+
+@app.route('/birthday')
+def birthday():
+
+    date_str = '2024-05-06'
+    date_format = "%Y-%m-%d"
+    given_date = datetime.datetime.strptime(date_str, date_format).date()
+    today = datetime.date.today()
+    delta = today - given_date
+    months1 = delta.days//30
+    daysmonths1 = delta.days % 30
+
+    date_str = '2024-09-06'
+    date_format = "%Y-%m-%d"
+    given_date = datetime.datetime.strptime(date_str, date_format).date()
+    today = datetime.date.today()
+    delta2 = today - given_date
+    months2 = delta2.days//30
+    daysmonths2 = delta2.days % 30
+
+    target_date = dt(2004, 4, 7)
+    days_since = (dt.now() - target_date).days
+    age = (days_since//365)-1
+
+
+    return render_template('birthday.html', firstday=delta.days, dateday=delta2.days, months1=months1, months2=months2
+                               , daysmonths1=daysmonths1, daysmonths2=daysmonths2, days_since=days_since, age=age)
+
 
 
 if __name__ == "__main__":
